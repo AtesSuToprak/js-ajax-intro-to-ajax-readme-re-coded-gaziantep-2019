08393fe4e24745a24f89670386486d188cd1230e
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-19 18:07:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -29,8 +29,7 @@ function getRepositories() {
 
 function showCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
+  const commitsList = `<ul>${commits.map(
       commit =>
         '<li><strong>' +
         commit.author.login +
