52d8a135a7246d9074fe09584a861880f51f0cb2
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-19 17:59:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,10 +6,10 @@
   req.send();
 }*/
 
-/*function showRepositories() {
+function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
-}*/
+}
  
 function getRepositories() {
   const req = new XMLHttpRequest();
