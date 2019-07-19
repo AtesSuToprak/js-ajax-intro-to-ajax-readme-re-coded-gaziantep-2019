6acda58232ad5d35b4af01dc56184d5afbc89068
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-19 18:13:11 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -20,12 +20,7 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
  
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/AtesSuToprak/repos');
-  req.send();
-}
+
 
 function getCommits(el) {
   const name = el.dataset.repo;
