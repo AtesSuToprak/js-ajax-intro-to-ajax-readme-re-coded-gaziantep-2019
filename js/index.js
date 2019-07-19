81fb7d6059b0b0dce04bd391b99afa80431645a5
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-19 17:55:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // your code here
+
+/*function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/AtesSuToprak/repos');
+  req.send();
+}*/
+
+/*function showRepositories() {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText);
+}*/
+ 
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/AtesSuToprak/repos');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
